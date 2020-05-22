f5711af797613fa65184725e3874bb6358d463e5
github username <github email address>
2020-05-22 13:57:22 -0400

    workout controller

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -7,4 +7,14 @@ module WorkoutsHelper
         @instructor=Instructor.find_or_create_by(name: name)
         self.instructor=@instructor
     end
+
+    def instructor_id_field(workout)
+        if workout.instructor.nil?
+            select_tag "workout[instructor_id]", 
+            options_from_collection_for_select(Instructor.all, :id, :name, include_blank:true)
+        else
+          hidden_field_tag "workout[instructor_id]", workout.instructor_id
+        end
+      end
+    
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,6 +3,12 @@ class Workout < ApplicationRecord
     belongs_to :instructor
     has_many :users, through: :bookings
 
-    
+    def instructor_name
+        self.instructor.name
+    end
+
+    def instructor_name=(name)
+        self.instructor.name=name
+    end
 
 end
